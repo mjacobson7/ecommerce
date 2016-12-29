f4d3e5dfcfa6b27c5f69ae39b57142de6510c79d
maxjacobson <max.jacobson@torchlms.com>
2016-12-28 20:39:50 -0700

    update server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ require('./config/passport')(app);
 require('./features/users/userRoutes.js')(app);
 require('./features/products/productRoutes.js')(app);
 
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
   console.log('The magic happens on port ' + port);
